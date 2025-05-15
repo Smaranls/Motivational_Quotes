cdc1670c6554eab5c1bdbce296f091aebf1bccbf
Smaran Lal Shrestha <smaran.sls@gmail.com>
2025-05-15 22:13:51 +0545

    Fix PEP8 and import order in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
-import sys
 import os
+import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, base_dir)
