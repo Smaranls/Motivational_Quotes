55547de55e3f2e09ef59e9e6a580819e458c67a1
Smaran Lal Shrestha <smaran.sls@gmail.com>
2025-05-15 22:02:11 +0545

    Fix line too long in api.py by breaking include_router line

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
+
 from app.api.routes import predictor
 
 router = APIRouter()
-router.include_router(predictor.router, prefix="/predictor", tags=["Predictor"])
+router.include_router(
+    predictor.router,
+    prefix="/predictor",
+    tags=["Predictor"]
+)
